dfca89ac2e25f0fe0580b8dfb362e42c2762cb11
=kleb0nas12 <=mr.rudzevicius@gmail.com>
2021-01-12 10:38:11 +0200

    extra test patch

--- test/test_sensor.py
+++ test/test_sensor.py
@@ -1,6 +1,7 @@
 from sensor_data.am2302_pi import AmSensor
 from ecxeptionlist import TooLowHighTempError
 import pytest
+from unittest.mock import patch
 
 class TestAmSensor:
 
@@ -9,22 +10,19 @@ class TestAmSensor:
         (2.34067,2.34),(32.07256,32.07),(-12.2589,-12.26)
     ])
     def test_transforming_float_data_to_2_decimal(self,number,expected):
-        '''Method to reduce float to 2 decimal numbers to
-        after the dot'''
+        '''Method to test reduce float to 2 decimal numbers to
+        after the decimal'''
         
         sensor = AmSensor()
 
         result = sensor.transform_sensor_data(number)
         assert result == expected
 
-    def test_abnormal_high_temp_error(self):
-        sensor = AmSensor()
-        with pytest.raises(TooLowHighTempError) as context:
-            result = sensor.transform_sensor_data(50)
-    
-    def test_abnormal_low_temp_error(self):
-        sensor = AmSensor()
-        with pytest.raises(TooLowHighTempError) as context:
-            result = sensor.transform_sensor_data(-40)
 
+
+    def test_mock_read_sensor(self, mocker):
+        mocker.patch('sensor_data.am2302_pi.Adafruit_DHT.read_retry', return_value = (20.589,88.689))
+        assert AmSensor() == (20.6,88.7)
+
+        
 

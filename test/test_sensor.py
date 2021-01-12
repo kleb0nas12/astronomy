462e5c13f18b6976f4bb904ddfc8049f21592539
=kleb0nas12 <=mr.rudzevicius@gmail.com>
2021-01-12 13:43:14 +0200

    extra test patch

--- test/test_sensor.py
+++ test/test_sensor.py
@@ -4,31 +4,42 @@ import pytest
 from unittest.mock import patch
 
 
-class TestAmSensor:
+# class TestAmSensor:
 
-    @pytest.mark.parametrize('number, expected', [
-        (2.34067, 2.34), (32.07256, 32.07), (-12.2589, -12.26)
-    ])
-    def test_transforming_float_data_to_2_decimal(self, number, expected):
-        '''Method to test reduce float to 2 decimal numbers to
-        after the decimal'''
+#     @pytest.mark.parametrize('number, expected', [
+#         (2.34067, 2.34), (32.07256, 32.07), (-12.2589, -12.26)
+#     ])
+#     def test_transforming_float_data_to_2_decimal(self, number, expected):
+#         '''Method to test reduce float to 2 decimal numbers to
+#         after the decimal'''
 
-        sensor = AmSensor()
+#         sensor = AmSensor()
 
-        result = sensor.transform_sensor_data(number)
-        assert result == expected
+#         result = sensor.transform_sensor_data(number)
+#         assert result == expected
 
-    # def test_mock_read_sensor(self, mocker):
-    #     mocker.patch('sensor_data.am2302_pi.Adafruit_DHT.read_retry',
-    #                  return_value=(20.589, 88.689))
-    #     _sensor = AmSensor()
-    #     assert _sensor.read_sensor_data() == (20.59, 88.69)
+#     # def test_mock_read_sensor(self, mocker):
+#     #     mocker.patch('sensor_data.am2302_pi.Adafruit_DHT.read_retry',
+#     #                  return_value=(20.589, 88.689))
+#     #     _sensor = AmSensor()
+#     #     assert _sensor.read_sensor_data() == (20.59, 88.69)
 
-    @patch('sensor_data.am2302_pi.Adafruit_DHT.read_retry', new = self.mocked_current_reading)
-    def test_main_function(self):
+#     @patch('sensor_data.am2302_pi.Adafruit_DHT.read_retry', new = mocked_current_reading)
+#     def test_main_function(self):
 
-        result = AmSensor.read_sensor_data()
-        truth = (20.59, 88.69)
-        assert result == truth
-    def mocked_current_reading(self):
-        return (20.589, 88.689)
+#         result = AmSensor.read_sensor_data()
+#         truth = (20.59, 88.69)
+#         assert result == truth
+#     def mocked_current_reading(self):
+#         return (20.589, 88.689)
+
+@patch('sensor_data.am2302_pi.Adafruit_DHT.read_retry', new=mocked_current_reading)
+def test_main_function(self):
+
+    result = AmSensor.read_sensor_data()
+    truth = (20.59, 88.69)
+    assert result == truth
+
+
+def mocked_current_reading(self):
+    return (20.589, 88.689)

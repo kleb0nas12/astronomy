b8f15b206026e04e614fd4baf04beaad2d536580
=kleb0nas12 <=mr.rudzevicius@gmail.com>
2021-01-12 19:08:38 +0200

    extra test patch

--- test/test_sensor.py
+++ test/test_sensor.py
@@ -3,25 +3,25 @@ from ecxeptionlist import TooLowHighTempError
 import pytest
 from unittest.mock import patch
 
-# class TestAmSensor:
+class TestAmSensor:
 
-#     @pytest.mark.parametrize('number, expected', [
-#         (2.34067, 2.34), (32.07256, 32.07), (-12.2589, -12.26)
-#     ])
-#     def test_transforming_float_data_to_2_decimal(self, number, expected):
-#         '''Method to test reduce float to 2 decimal numbers to
-#         after the decimal'''
+    @pytest.mark.parametrize('number, expected', [
+        (2.34067, 2.34), (32.07256, 32.07), (-12.2589, -12.26)
+    ])
+    def test_transforming_float_data_to_2_decimal(self, number, expected):
+        '''Method to test reduce float to 2 decimal numbers to
+        after the decimal'''
 
-#         sensor = AmSensor()
+        sensor = AmSensor()
 
-#         result = sensor.transform_sensor_data(number)
-#         assert result == expected
+        result = sensor.transform_sensor_data(number)
+        assert result == expected
 
-#     def test_mock_read_sensor(self, mocker):
-#         mocker.patch('am2302_pi.Adafruit_DHT.read_retry',
-#                      return_value=(20.589, 28.689), autospec=True)
-#         _sensor = AmSensor()
-#         assert _sensor.read_sensor_data() == (20.59, 28.69)
+    def test_mock_read_sensor(self, mocker):
+        mocker.patch('am2302_pi.Adafruit_DHT.read_retry',
+                     return_value=(20.589, 28.689), autospec=True)
+        _sensor = AmSensor()
+        assert _sensor.read_sensor_data() == (20.59, 28.69)
 
 # #     @patch('sensor_data.am2302_pi.Adafruit_DHT.read_retry', new = mocked_current_reading)
 # #     def test_main_function(self):
@@ -42,14 +42,14 @@ from unittest.mock import patch
 #     assert result == truth
 
 
-def current_reading():
-    return (20.589, 28.689)
+# def current_reading():
+#     return (20.589, 28.689)
 
-@patch('am2302_pi.Adafruit_DHT.read_retry')
-def test_main_function(mock):
-    mock.return_value = current_reading()
-    data= AmSensor()
-    result = data.read_sensor_data()
-    truth = (20.59, 28.69)
-    assert result == truth
+# @patch('am2302_pi.Adafruit_DHT.read_retry')
+# def test_main_function(mock):
+#     mock.return_value = current_reading()
+#     data= AmSensor()
+#     result = data.read_sensor_data()
+#     truth = (20.59, 28.69)
+#     assert result == truth
 

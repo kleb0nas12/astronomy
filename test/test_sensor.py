a5746de9638391d972ee7bf68972a7a1a5d1f090
=kleb0nas12 <=mr.rudzevicius@gmail.com>
2021-01-12 19:16:36 +0200

    extra test patch done

--- test/test_sensor.py
+++ test/test_sensor.py
@@ -17,39 +17,12 @@ class TestAmSensor:
         result = sensor.transform_sensor_data(number)
         assert result == expected
 
+
+
     def test_mock_read_sensor(self, mocker):
+        ''' Test sensor temp and humidy data'''
         mocker.patch('sensor_data.am2302_pi.Adafruit_DHT.read_retry',
                      return_value=(20.589, 28.689), autospec=True)
         _sensor = AmSensor()
         assert _sensor.read_sensor_data() == (20.59, 28.69)
-
-# #     @patch('sensor_data.am2302_pi.Adafruit_DHT.read_retry', new = mocked_current_reading)
-# #     def test_main_function(self):
-
-# #         result = AmSensor.read_sensor_data()
-# #         truth = (20.59, 88.69)
-# #         assert result == truth
-# #     def mocked_current_reading(self):
-# #         return (20.589, 88.689)
-# def current_reading():
-#     return (20.589, 88.689)
-
-# @patch('sensor_data.am2302_pi.Adafruit_DHT.read_retry', new=current_reading)
-# def test_main_function():
-#     data= AmSensor()
-#     result = data.read_sensor_data()
-#     truth = (20.59, 88.69)
-#     assert result == truth
-
-
-# def current_reading():
-#     return (20.589, 28.689)
-
-# @patch('am2302_pi.Adafruit_DHT.read_retry')
-# def test_main_function(mock):
-#     mock.return_value = current_reading()
-#     data= AmSensor()
-#     result = data.read_sensor_data()
-#     truth = (20.59, 28.69)
-#     assert result == truth
 

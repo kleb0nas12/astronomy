a5ef4f75bb5375d71d7403666a3d3895d81b7360
=kleb0nas12 <=mr.rudzevicius@gmail.com>
2021-01-13 16:23:09 +0200

    temp

--- sensor_data/am2302_pi.py
+++ sensor_data/am2302_pi.py
@@ -16,16 +16,16 @@ class AmSensor(object):
     def read_sensor_data(self) -> tuple:
         DHT_SENSOR = Adafruit_DHT.DHT22
         DHT_PIN = 18
-        try:
-            humidity, temp = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
-            humidity = self.transform_sensor_data(humidity)
-            temp = self.transform_sensor_data(temp)
-            print(humidity, temp)
-            if int(temp) < -30 or int(temp) > 60:
-                raise manual_exceptions.TooLowHighTempError(temp)
-            return humidity, temp
-        except Exception as e:
-            print(f'Problem has appeared with the exception {e}')
+        
+        humidity, temp = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
+        humidity = self.transform_sensor_data(humidity)
+        temp = self.transform_sensor_data(temp)
+        print(humidity, temp)
+        if int(temp) < -30 or int(temp) > 60:
+            raise manual_exceptions.TooLowHighTempError(temp)
+        return humidity, temp
+        # except Exception as e:
+        #     print(f'Problem has appeared with the exception {e}')
 
     # transforming data to the nearest second number after the decimal
 

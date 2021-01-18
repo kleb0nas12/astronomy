18a6501b8d52beebe226c82481cfce2f032bd869
=kleb0nas12 <=mr.rudzevicius@gmail.com>
2021-01-18 12:30:49 +0200

    unittest part of the code

--- test/test_sensor.py
+++ test/test_sensor.py
@@ -39,6 +39,7 @@ class TestAmSensor:
 
 
     ### Now without pytest-mock , but using unittest-mock.
+    @staticmethod
     def mocked_adafruit_sensor_data(self):
         ''' Mock our adafruit sensor function to return some data values'''
         return 20.589, 28.689
@@ -52,7 +53,7 @@ class TestAmSensor:
         assert result == truth
 
 
-    @patch('sensor_data.am2302_pi.Adafruit_DHT.read_retry', new = TestAmSensor().mocked_adafruit_sensor_data)
+    @patch('sensor_data.am2302_pi.Adafruit_DHT.read_retry', new = mocked_adafruit_sensor_data)
     def test_mock_read_sensor_with_unittest(self):
         _sensor = AmSensor()
         assert _sensor.read_sensor_data() == (20.59, 28.69)
